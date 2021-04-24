c318d9e799f5b817d6d47300945a2a4fc805f672
William Infante <williaminfante@gmail.com>
2021-04-24 15:31:43 +1000

    feat: add template

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/williaminfante/go_test_starter
+
+go 1.16
---
+++ starter.go
@@ -0,0 +1,10 @@
+package starter
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
